481c626881f9cf3932c2f097574002fdd94cf38a
Tony Rowan <tony-rowan@cookpad.com>
2021-08-26 10:11:44 +0100

    Add a selected state

--- app/javascript/controllers/navigation_controller.js
+++ app/javascript/controllers/navigation_controller.js
@@ -1,15 +1,29 @@
 import { Controller } from "stimulus"
 
 export default class extends Controller {
-  static targets = ['content', 'loading'];
+  static targets = ['content', 'loading', 'link'];
 
   displayLoading(event) {
     this.loadingTarget.classList.remove('hidden');
     this.contentTarget.classList.add('hidden');
+
+    let value = event.detail.url;
+
+    this.updateLinks(value);
   }
 
   displayContent() {
     this.loadingTarget.classList.add('hidden');
     this.contentTarget.classList.remove('hidden');
+  }
+
+  updateLinks(value) {
+    this.linkTargets.forEach((link) => {
+
+      link.classList.remove('bg-blue-400');
+      if (link.href === value) {
+        link.classList.add('bg-blue-400');
+      }
+    })
   }
 }
