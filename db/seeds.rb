ac7a82437ca7a42750c7c80a3460482a8cf01a34
Tony Rowan <tony-rowan@cookpad.com>
2021-08-27 15:54:49 +0100

    Some better test data

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,21 +8,57 @@
 
 Movie.create([
   {
-    title: "Star Wars: A New Hope",
-    image: "https://static0.colliderimages.com/wordpress/wp-content/uploads/2015/12/star-wars-a-new-hope-poster.jpg",
-    summary: "The begining of the world's greatest media franchise",
+    title: "Star Wars: Episode IV - A New Hope",
+    image: "https://m.media-amazon.com/images/M/MV5BNzVlY2MwMjktM2E4OS00Y2Y3LWE3ZjctYzhkZGM3YzA1ZWM2XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_.jpg",
+    summary: "Luke Skywalker joins forces with a Jedi Knight, a cocky pilot, a Wookiee and two droids to save the galaxy from the Empire's world-destroying battle station, while also attempting to rescue Princess Leia from the mysterious Darth Vader.",
     director: "George Lucas",
-    length: "111 mins",
+    length: "2h 1min",
     genre: "Sci-Fi",
-    year: "1974"
+    year: "1977"
+  },
+  {
+    title: "Star Wars: Episode V - The Empire Strikes Back",
+    image: "https://m.media-amazon.com/images/M/MV5BYmU1NDRjNDgtMzhiMi00NjZmLTg5NGItZDNiZjU5NTU4OTE0XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_.jpg",
+    summary: "After the Rebels are brutally overpowered by the Empire on the ice planet Hoth, Luke Skywalker begins Jedi training with Yoda, while his friends are pursued across the galaxy by Darth Vader and bounty hunter Boba Fett.",
+    director: "Irvin Kershner",
+    length: "2h 4min",
+    genre: "Sci-Fi",
+    year: "1980"
+  },
+  {
+    title: "Star Wars: Episode VI - Return of the Jedi",
+    image: "https://m.media-amazon.com/images/M/MV5BOWZlMjFiYzgtMTUzNC00Y2IzLTk1NTMtZmNhMTczNTk0ODk1XkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_.jpg",
+    summary: "After a daring mission to rescue Han Solo from Jabba the Hutt, the Rebels dispatch to Endor to destroy the second Death Star. Meanwhile, Luke struggles to help Darth Vader back from the dark side without falling into the Emperor's trap.",
+    director: "Richard Marquand",
+    length: "2h 11min",
+    genre: "Sci-Fi",
+    year: "1983"
   },
   {
     title: "Lord of the Rings: The Fellowship of the Ring",
-    image: "https://1.bp.blogspot.com/-3Qr51FbCt7A/UP-ccYH1e0I/AAAAAAAAA2w/2aM1a9ZcBnk/s1600/the-lord-of-the-rings-fellowship-of-the-rings_1.jpg",
-    summary: "The first installment of the greatest triloogy of films ever made",
+    image: "https://m.media-amazon.com/images/M/MV5BN2EyZjM3NzUtNWUzMi00MTgxLWI0NTctMzY4M2VlOTdjZWRiXkEyXkFqcGdeQXVyNDUzOTQ5MjY@._V1_.jpg",
+    summary: "A meek Hobbit from the Shire and eight companions set out on a journey to destroy the powerful One Ring and save Middle-earth from the Dark Lord Sauron.",
     director: "Peter Jackson",
-    length: "About 4 years, if the producers hadn't made him cut it down",
+    length: "2h 58min",
     genre: "Fantasy",
     year: "2001"
   },
+  {
+    title: "Lord of the Rings: The Two Towers",
+    image: "https://m.media-amazon.com/images/M/MV5BZGMxZTdjZmYtMmE2Ni00ZTdkLWI5NTgtNjlmMjBiNzU2MmI5XkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_.jpg",
+    summary: "While Frodo and Sam edge closer to Mordor with the help of the shifty Gollum, the divided fellowship makes a stand against Sauron's new ally, Saruman, and his hordes of Isengard.",
+    director: "Peter Jackson",
+    length: "2h 59min",
+    genre: "Fantasy",
+    year: "2002"
+  },
+  {
+    title: "Lord of the Rings: The Return of the King",
+    image: "https://m.media-amazon.com/images/M/MV5BNzA5ZDNlZWMtM2NhNS00NDJjLTk4NDItYTRmY2EwMWZlMTY3XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_.jpg",
+    summary: "Gandalf and Aragorn lead the World of Men against Sauron's army to draw his gaze from Frodo and Sam as they approach Mount Doom with the One Ring.",
+    director: "Peter Jackson",
+    length: "3h 21min",
+    genre: "Fantasy",
+    year: "2003"
+  }
 ])
