e647cfe07fd5677d73b477386ea0313d3d1fe635
Tony Rowan <tony-rowan@cookpad.com>
2022-01-07 16:37:50 +0000

    Fill the star bar as the user hovers over it

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -7,5 +7,8 @@ import { application } from "./application";
 import NavigationController from "./navigation_controller.js";
 application.register("navigation", NavigationController);
 
-import SubmitFormController from "./submit_form_controller.js";
-application.register("submit-form", SubmitFormController);
+import StarBarController from "./star_bar_controller.js"
+application.register("star-bar", StarBarController)
+
+import SubmitFormController from "./submit_form_controller.js"
+application.register("submit-form", SubmitFormController)
---
+++ app/javascript/controllers/star_bar_controller.js
@@ -0,0 +1,24 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  static targets = ["star"];
+  static classes = ["hover"];
+
+  enter(event) {
+    this._fillToStar(event.params.starIndex);
+  }
+
+  leave() {
+    this._fillToStar(-1);
+  }
+
+  _fillToStar(star) {
+     this.starTargets.forEach((target, index) => {
+      if (index <= star) {
+        target.classList.add(this.hoverClass);
+      } else {
+        target.classList.remove(this.hoverClass);
+      }
+    });
+  }
+}
