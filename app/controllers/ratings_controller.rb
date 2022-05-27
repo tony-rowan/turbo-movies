40305ca36dd690978e108db437ebf5aacc91d8eb
Tony Rowan <tony-rowan@cookpad.com>
2022-05-27 15:57:11 +0100

    Use flash to display toast messages

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -1,10 +1,11 @@
 class RatingsController < ApplicationController
   def update
     @movie = Movie.find(params[:movie_id])
-    @toast = if @movie.update(rating: params[:rating])
-      Toast.new(type: :success, message: "Rating Added!")
+
+    if @movie.update(rating: params[:rating])
+      flash.now[:notice] = "Quote was successfully updated."
     else
-      Toast.new(type: :error, message: "Could not apply rating, please try again later")
+      flash.now[:error] = "Could not apply rating, please try again later"
     end
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  FLASH_CSS_CLASSES = {
+    notice: "bg-slate-600",
+    error: "bg-red-500"
+  }.with_indifferent_access.freeze
+
+  def flash_css_class(key)
+    "flash-animation p-4 rounded text-white #{FLASH_CSS_CLASSES[key]}"
+  end
 end
