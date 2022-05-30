3d7135a5a94b872858aee97b6cb8635b296baa1c
Tony Rowan <tony-rowan@cookpad.com>
2022-05-30 11:59:58 +0100

    Improve ability to work without javascript

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -2,10 +2,14 @@ class RatingsController < ApplicationController
   def update
     @movie = Movie.find(params[:movie_id])
 
-    if @movie.update(rating: params[:rating])
-      flash.now[:notice] = "Quote was successfully updated."
-    else
-      flash.now[:error] = "Could not apply rating, please try again later"
+    respond_to do |format|
+      if @movie.update(rating: params[:rating])
+        format.html { redirect_to @movie, notice: "Rating Applied!" }
+        format.turbo_stream { flash.now[:notice] = "Rating Applied!" }
+      else
+        format.html { redirect_to @movie, notice: "Could not apply rating, please try again later" }
+        format.turbo_steam { flash.now[:error] = "Could not apply rating, please try again later" }
+      end
     end
   end
 end
